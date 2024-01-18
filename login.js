4b7c6e7658a58908da42f456b5bc4cdc2e97d4f6
OmarAstro <145227504+OmarAstro@users.noreply.github.com>
2024-01-18 23:45:31 +0200

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  if ((password === "26/5", username === "toka")) {
+  if ((password === "26/5" && username === "toka")) {
     window.location.href = "index1.html";
   } else {
     alert("كلمة المرور غير صحيحة");
