4d82d68444ccfae3b6d264e5331e1f659b8fac4f
OmarAstro <145227504+OmarAstro@users.noreply.github.com>
2024-01-18 23:30:39 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if ((password === "26/5", username === "toka")) {
+    window.location.href = "index.html";
+  } else {
+    alert("كلمة المرور غير صحيحة");
+  }
+}
